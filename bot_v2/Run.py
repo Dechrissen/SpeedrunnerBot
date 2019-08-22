2fd0808a24faae3a985bcc1885c9bc01a319dfa9
Derek Andersen <41839810+Dechrissen@users.noreply.github.com>
2019-08-22 18:48:06 -0400

    Update Run.py

--- bot_v2/Run.py
+++ bot_v2/Run.py
@@ -1,5 +1,6 @@
 import string
 import time
+import math
 import urllib.request
 from urllib.request import urlopen
 from json import loads
@@ -109,7 +110,7 @@ def personalBest(input):
             place = None
             time_in_sec = None
             for cat in lst['data']:
-                if cat['category']['data']['name'] == category_title and cat['game']['data']['names']['international'].lower() == game_title and cat['platform']['data']['name'] == platform_name:
+                if cat['category']['data']['name'] == category_title and cat['game']['data']['names']['international'].lower() == game_title and cat['platform']['data']['name'] == platform_title:
                     time_in_sec = int(cat['run']['times']['realtime_t'])
                     place = cat['place']
                     break
